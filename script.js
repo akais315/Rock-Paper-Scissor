dafc9dd581c00b17b6a39f3b34a89741b4457d98
akais315 <akais315@gmail.com>
2022-06-23 19:38:54 +0200

    Add playRound function

--- script.js
+++ script.js
@@ -15,4 +15,33 @@ function computerPlay(){
     return choice;
 }
 
-computerPlay();
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toUpperCase();
+    if(playerSelection === "ROCK"){
+        if(computerSelection === "Paper"){
+            return "You Lose! Paper beats Rock";
+        } else if(computerSelection === "Scissor"){
+            return "You Win! Rock beats Scissor";
+        } else {
+            return "Draw ! Rock vs Rock has no winner"
+        }
+    } else if(playerSelection === "PAPER"){
+        if(computerSelection === "Scissor"){
+            return "You Lose! Scissor beats Paper";
+        } else if(computerSelection === "Rock"){
+            return "You Win! Paper beats Rock";
+        } else {
+            return "Draw ! Paper vs Paper has no winner"
+        }
+    } else if(playerSelection === "SCISSOR"){
+        if(computerSelection === "Rock"){
+            return "You Lose! Rock beats Scissor";
+        } else if(computerSelection === "Paper"){
+            return "You Win! Scissor beats Paper";
+        } else {
+            return "Draw ! Scissor vs Rock has no winner"
+        }
+    }
+}
+
+console.log(playRound(prompt("Choose Rock, Paper or Scissor"),computerPlay()));
