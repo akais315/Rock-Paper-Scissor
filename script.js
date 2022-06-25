3a0f2cb6f3a26642753cf4b4b6247797abc92e56
akais315 <akais315@gmail.com>
2022-06-25 23:06:16 +0200

    Add Buttons and UI and rework logic

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+let playerResult = 0;
+let computerResult = 0;
+
+const rock = document.querySelector('#rock');
+rock.addEventListener('click', function() {playRound("ROCK", computerPlay());});
+const paper = document.querySelector('#paper');
+paper.addEventListener('click', function() {playRound("PAPER", computerPlay());});
+const scissor = document.querySelector('#scissor');
+scissor.addEventListener('click', function() {playRound("SCISSOR", computerPlay());});
+
+function checkResult(){
+    if(playerResult >= 5){
+        document.getElementById("result").innerHTML = ("You Won the game!");
+    }else if(computerResult >= 5){
+        document.getElementById("result").innerHTML = ("You Lost, the computer won the game!");
+    }
+}
+
+function drawScore(){
+    document.getElementById("score").innerHTML = playerResult + " - " + computerResult;
+}
+
 function computerPlay(){
     let rand = Math.floor(Math.random() * 3);
     let choice
@@ -16,55 +38,41 @@ function computerPlay(){
 }
 
 function playRound(playerSelection, computerSelection){
-    playerSelection = playerSelection.toUpperCase();
-    if(playerSelection === "ROCK"){
-        if(computerSelection === "Paper"){
-            console.log("You Lose! Paper beats Rock");
-            return -1;
-        } else if(computerSelection === "Scissor"){
-            console.log("You Win! Rock beats Scissor");
-            return 1
-        } else {
-            console.log("Draw ! Rock vs Rock has no winner");
-            return 0
+    if(playerResult < 5 && computerResult < 5){
+        playerSelection = playerSelection.toUpperCase();
+        if(playerSelection === "ROCK"){
+            if(computerSelection === "Paper"){
+                document.getElementById("result").innerHTML = "You Lose! Paper beats Rock";
+                computerResult += 1;
+            } else if(computerSelection === "Scissor"){
+                document.getElementById("result").innerHTML = "You Win! Rock beats Scissor";
+                playerResult += 1;
+            } else {
+                document.getElementById("result").innerHTML = "Draw ! Rock vs Rock has no winner";
+            }
+        } else if(playerSelection === "PAPER"){
+            if(computerSelection === "Scissor"){
+                document.getElementById("result").innerHTML = ("You Lose! Scissor beats Paper");
+                computerResult += 1;
+            } else if(computerSelection === "Rock"){
+                document.getElementById("result").innerHTML = ("You Win! Paper beats Rock");
+                playerResult += 1;
+            } else {
+                document.getElementById("result").innerHTML = ("Draw ! Paper vs Paper has no winner");
+            }
+        } else if(playerSelection === "SCISSOR"){
+            if(computerSelection === "Rock"){
+                document.getElementById("result").innerHTML = ("You Lose! Rock beats Scissor");
+                computerResult += 1;
+            } else if(computerSelection === "Paper"){
+                document.getElementById("result").innerHTML = ("You Win! Scissor beats Paper");
+                playerResult += 1;
+            } else {
+                document.getElementById("result").innerHTML = ("Draw ! Scissor vs Scissor has no winner");
+            }
         }
-    } else if(playerSelection === "PAPER"){
-        if(computerSelection === "Scissor"){
-            console.log("You Lose! Scissor beats Paper");
-            return -1;
-        } else if(computerSelection === "Rock"){
-            console.log("You Win! Paper beats Rock");
-            return 1;
-        } else {
-            console.log("Draw ! Paper vs Paper has no winner");
-            return 0;
-        }
-    } else if(playerSelection === "SCISSOR"){
-        if(computerSelection === "Rock"){
-            console.log("You Lose! Rock beats Scissor");
-            return -1;
-        } else if(computerSelection === "Paper"){
-            console.log("You Win! Scissor beats Paper");
-            return 1;
-        } else {
-            console.log("Draw ! Scissor vs Rock has no winner");
-            return 0;
-        }
+        drawScore();
+        checkResult();
     }
 }
 
-function game(){
-    let result = 0;
-    for(let i = 0; i < 5; i++){
-        result += playRound(prompt("Choose Rock, Paper or Scissor"),computerPlay());
-    }
-    if(result > 0){
-        console.log("You Won!");
-    }else if(result < 0){
-        console.log("You Lost!");
-    } else{
-        console.log("Draw, nobody wins!")
-    }
-}
-
-game();
